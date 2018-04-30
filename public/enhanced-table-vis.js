12cf0975fc17a0184bab5217147ca839aa15d90c
fbaligand <fbaligand@gmail.com>
2018-04-30 19:04:33 +0200

    add "Filter as you type" option

--- public/enhanced-table-vis.js
+++ public/enhanced-table-vis.js
@@ -54,6 +54,7 @@ function EnhancedTableVisProvider(Private) {
         showFilterBar: false,
         filterCaseSensitive: false,
         filterBarHideable: false,
+        filterAsYouType: false,
         filterBarWidth: '25%'
       },
     },
