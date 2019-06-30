909ebced72ed62d74695841e035e874b7a7e4bee
fbaligand <fbaligand@gmail.com>
2019-06-30 23:22:44 +0200

    top hits agg: add a hack to support string fields

--- public/enhanced-table-vis-hack.js
+++ public/enhanced-table-vis-hack.js
@@ -1,5 +1,8 @@
 import { uiModules } from  'ui/modules';
+import _ from 'lodash';
 import chrome from 'ui/chrome';
+import { aggTypeFieldFilters } from 'ui/agg_types/param_types/filter';
+import { topHitMetricAgg } from 'ui/agg_types/metrics/top_hit';
 
 const appId = chrome.getApp().id;
 
@@ -45,5 +48,34 @@ if (appId === 'kibana') {
         return $delegate;
       });
 
+      // Enable string fields in top hit aggregation for enhanced-table plugin
+      const topHitFilter = _.find(Array.from(aggTypeFieldFilters.filters), (filter) => {
+        const field = {type: 'string'};
+        const fieldParamType = '';
+        const aggConfig = {type: {name: 'top_hit'}};
+        const tableVis = {type: {name: 'table'}};
+        const enhancedTableVis = {type: {name: 'enhanced-table'}};
+        try {
+          return filter(field, fieldParamType, aggConfig, tableVis) && !filter(field, fieldParamType, aggConfig, enhancedTableVis);
+        }
+        catch (e) {
+          return false;
+        }
+      });
+      
+      if (topHitFilter !== undefined) {
+        aggTypeFieldFilters.filters.delete(topHitFilter);
+        aggTypeFieldFilters.addFilter(
+          (field, fieldParamType, aggConfig, vis) => {
+            return vis.type.name === 'enhanced-table' || topHitFilter(field, fieldParamType, aggConfig, vis);
+          }
+        );
+        
+        const concatOption = topHitMetricAgg.params.filter(param => param.name === 'aggregate')[0]
+          .options.filter(option => option.val === 'concat')[0];
+        const isCompatibleVisOriginalMethod = concatOption.isCompatibleVis;
+        concatOption.isCompatibleVis = (name) => name === 'enhanced-table' || isCompatibleVisOriginalMethod(name);
+      }
+      
     });
 }
