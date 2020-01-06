c9a5cbc421e8ef019c1cd38261d7f848c49b3d3a
fbaligand <fbaligand@gmail.com>
2020-01-07 00:14:40 +0100

    version 1.7.0

--- public/enhanced-table-vis.js
+++ public/enhanced-table-vis.js
@@ -78,7 +78,7 @@ function EnhancedTableVisProvider(Private) {
     name: 'enhanced-table',
     title: 'Enhanced Table',
     image,
-    description: 'Same functionality than Data Table, but with enhanced features like computed columns and filter bar.',
+    description: 'Same functionality than Data Table, but with enhanced features like computed columns, filter bar and pivot table.',
     category: CATEGORY.DATA,
     template: tableVisTemplate,
     params: {
