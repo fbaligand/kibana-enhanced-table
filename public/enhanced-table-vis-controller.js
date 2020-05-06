73f0b44222dd6c89da6151a0e596d0de1a55e5a4
fbaligand <fbaligand@gmail.com>
2020-05-06 21:52:01 +0200

    use " around formula in error messages (instead of ')

--- public/enhanced-table-vis-controller.js
+++ public/enhanced-table-vis-controller.js
@@ -106,7 +106,7 @@ module.controller('EnhancedTableVisController', function ($scope, $element, Priv
       return getOriginalColIndex(columnIndex, splitColIndex);
     }
     else {
-      throw new EnhancedTableError(`In ${inputType} '${input}', column with label '${colTitle}' does not exist`);
+      throw new EnhancedTableError(`In ${inputType} "${input}", column with label '${colTitle}' does not exist`);
     }
   };
 
@@ -132,7 +132,7 @@ module.controller('EnhancedTableVisController', function ($scope, $element, Priv
       let colIndex = parseInt(regexMatch[1]);
       if (colIndex >= currentCol) {
         colIndex = getOriginalColIndex(colIndex, splitColIndex);
-        throw new EnhancedTableError(`In computed column '${inputFormula}', column number ${colIndex} does not exist`);
+        throw new EnhancedTableError(`In computed column "${inputFormula}", column number ${colIndex} does not exist`);
       }
       formulaParamsCols.push(colIndex);
     }
@@ -153,7 +153,7 @@ module.controller('EnhancedTableVisController', function ($scope, $element, Priv
 
     // check 'sumSplitCols/countSplitCols' functions condition
     if ((realFormula.indexOf('sumSplitCols') !== -1 || realFormula.indexOf('countSplitCols') !== -1) && splitColIndex === -1) {
-      throw new EnhancedTableError(`In computed column '${inputFormula}', sumSplitCols() and countSplitCols() functions must be used with a 'Split cols' bucket`);
+      throw new EnhancedTableError(`In computed column "${inputFormula}", sumSplitCols() and countSplitCols() functions must be used with a "Split cols" bucket`);
     }
 
     // extract formula param totals
@@ -162,7 +162,7 @@ module.controller('EnhancedTableVisController', function ($scope, $element, Priv
       let colIndex = parseInt(regexMatch[1]);
       if (colIndex >= currentCol) {
         colIndex = getOriginalColIndex(colIndex, splitColIndex);
-        throw new EnhancedTableError(`In computed column '${inputFormula}', column number ${colIndex} does not exist`);
+        throw new EnhancedTableError(`In computed column "${inputFormula}", column number ${colIndex} does not exist`);
       }
       formulaParamsTotals.push(colIndex);
     }
@@ -260,7 +260,7 @@ module.controller('EnhancedTableVisController', function ($scope, $element, Priv
       };
     }
     catch (e) {
-      throw new EnhancedTableError(`Invalid computed column formula '${inputFormula}' (${e.message})`);
+      throw new EnhancedTableError(`Invalid computed column formula "${inputFormula}" (${e.message})`);
     }
   };
 
@@ -835,7 +835,7 @@ module.controller('EnhancedTableVisController', function ($scope, $element, Priv
         if (splitColIndex != -1) {
           const lastBucketIndex = _.findLastIndex(firstTable.columns, col => col.aggConfig.schema.group === 'buckets');
           if (splitColIndex !== lastBucketIndex) {
-            notifyError('\'Split Cols\' bucket must be the last one');
+            notifyError('"Split Cols" bucket must be the last one');
             return;
           }
         }
