4d2d114eaaa19055673822a0c641f07f0d7dc2ef
fbaligand <fbaligand@gmail.com>
2020-09-05 10:35:37 +0200

    add 'Add row number column' option feature

--- public/enhanced-table-vis.js
+++ public/enhanced-table-vis.js
@@ -66,6 +66,7 @@ function EnhancedTableVisTypeProvider(Private) {
         computedColsPerSplitCol: false,
         hideExportLinks: false,
         stripedRows: false,
+        addRowNumberColumn: false,
         showFilterBar: false,
         filterCaseSensitive: false,
         filterBarHideable: false,
