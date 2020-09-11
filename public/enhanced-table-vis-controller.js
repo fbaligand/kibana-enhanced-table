47cc5f39a0f89de401495016b689135d607af2f5
fbaligand <fbaligand@gmail.com>
2020-09-11 23:44:36 +0200

    fix col(colRef) function for undefined cols

--- public/enhanced-table-vis-controller.js
+++ public/enhanced-table-vis-controller.js
@@ -217,7 +217,8 @@ function EnhancedTableVisController ($scope, config) {
         }
         if (colIndex < currentCol) {
           colIndex = getRealColIndex(colIndex, splitColIndex);
-          return row[colIndex].value;
+          const colValue = row[colIndex].value;
+          return colValue !== undefined ? colValue : defaultValue;
         }
         else {
           return defaultValue;
