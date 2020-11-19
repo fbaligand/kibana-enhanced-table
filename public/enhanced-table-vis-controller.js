e448dfb1e37ed87fccf0a18cf2217865f7071b53
fbaligand <fbaligand@gmail.com>
2020-11-19 09:07:21 +0100

    fix report generation when there is no results (#93)

--- public/enhanced-table-vis-controller.js
+++ public/enhanced-table-vis-controller.js
@@ -930,6 +930,7 @@ module.controller('EnhancedTableVisController', function ($scope, Private, confi
         // no data to display
         if (totalHits === 0 || firstTable === null) {
           $scope.hasSomeRows = false;
+          $scope.renderComplete();
           return;
         }
 
