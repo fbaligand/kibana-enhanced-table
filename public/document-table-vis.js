fc3d553ca9b07e4741402a7cef1f0772b8a5fb41
fbaligand <fbaligand@gmail.com>
2020-11-22 23:57:04 +0100

    add 'CSV export with total row' feature

--- public/document-table-vis.js
+++ public/document-table-vis.js
@@ -55,6 +55,7 @@ export const documentTableVisTypeDefinition = {
       computedColumns: [],
       computedColsPerSplitCol: false,
       hideExportLinks: false,
+      csvExportWithTotal: false,
       stripedRows: false,
       addRowNumberColumn: false,
       csvEncoding: 'utf-8',
--- public/enhanced-table-vis.js
+++ public/enhanced-table-vis.js
@@ -55,6 +55,7 @@ export const enhancedTableVisTypeDefinition = {
       computedColumns: [],
       computedColsPerSplitCol: false,
       hideExportLinks: false,
+      csvExportWithTotal: false,
       stripedRows: false,
       addRowNumberColumn: false,
       csvEncoding: 'utf-8',
