5e8756f9f18753e66e001d7aad932981c0d8bea9
fbaligand <fbaligand@gmail.com>
2020-11-28 12:43:50 +0100

    fix hidden filter bar when a global filter is added

--- public/enhanced-table-vis-controller.js
+++ public/enhanced-table-vis-controller.js
@@ -751,6 +751,15 @@ module.controller('EnhancedTableVisController', function ($scope, Private, confi
     }
   };
 
+  const hasRows = function(table) {
+    if (table.tables) {
+      return table.tables.some(hasRows);
+    }
+    else {
+      return table.rows.length > 0;
+    }
+  };
+
   // filter scope methods
   $scope.doFilter = function () {
     $scope.activeFilter = $scope.vis.filterInput;
@@ -817,10 +826,7 @@ module.controller('EnhancedTableVisController', function ($scope, Private, confi
       }
 
       // check if there are rows to display
-      const hasSomeRows = tableGroups.tables.some(function haveRows(table) {
-        if (table.tables) return table.tables.some(haveRows);
-        return table.rows.length > 0;
-      });
+      const hasSomeRows = hasRows(tableGroups);
 
       // optimize space under table
       const showPagination = hasSomeRows && params.perPage && shouldShowPagination(tableGroups.tables, params.perPage);
@@ -867,8 +873,9 @@ module.controller('EnhancedTableVisController', function ($scope, Private, confi
       if ($scope.esResponse && !$scope.esResponse.enhanced) {
 
         // init tableGroups
-        $scope.tableGroups = null;
         $scope.hasSomeRows = null;
+        $scope.hasSomeData = null;
+        $scope.tableGroups = null;
         const tableGroups = $scope.esResponse;
         const totalHits = $scope.vis.searchSource.rawResponse.hits.total;
         tableGroups.enhanced = true;
@@ -887,6 +894,7 @@ module.controller('EnhancedTableVisController', function ($scope, Private, confi
         // no data to display
         if (totalHits === 0 || firstTable === null) {
           $scope.hasSomeRows = false;
+          $scope.hasSomeData = false;
           $scope.renderComplete();
           return;
         }
@@ -951,6 +959,9 @@ module.controller('EnhancedTableVisController', function ($scope, Private, confi
             }
           });
         }
+
+        // compute if there is some data before filtering
+        $scope.hasSomeData = hasRows(tableGroups);
 
         // process filter bar
         processFilterBarAndRefreshTable();
