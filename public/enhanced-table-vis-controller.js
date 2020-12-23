495a0696bafb82652222790021884bfa8c8ceebf
fbaligand <fbaligand@gmail.com>
2020-12-23 13:57:05 +0100

    add 'timeRange' variable to computed expressions

--- public/enhanced-table-vis-controller.js
+++ public/enhanced-table-vis-controller.js
@@ -20,6 +20,7 @@
 import _ from 'lodash';
 
 import { computeColumnTotal } from './column_total_computer';
+import { computeTimeRange } from './time_range_computer';
 import AggConfigResult from './data_load/agg_config_result';
 
 import { npStart } from 'ui/new_platform';
@@ -45,10 +46,10 @@ function EnhancedTableVisController ($scope, Private, config) {
 
   // controller methods
 
-  const createTemplateContext = function (column, row, totalHits, table) {
+  const createTemplateContext = function (table, column, row, totalHits, timeRange) {
 
     // inject column value references
-    const templateContext = { total: totalHits };
+    const templateContext = { total: totalHits, timeRange: timeRange };
     _.forEach(column.template.paramsCols, function (templateParamCol) {
       templateContext[`col${templateParamCol}`] = row[templateParamCol].value;
     });
@@ -282,9 +283,9 @@ function EnhancedTableVisController ($scope, Private, config) {
     }
   };
 
-  const computeFormulaValue = function (formula, row, totalHits, table, cellValue) {
+  const computeFormulaValue = function (formula, table, row, totalHits, timeRange, cellValue) {
     try {
-      const formulaParams = { total: totalHits, row: row, value: cellValue };
+      const formulaParams = { total: totalHits, row: row, timeRange: timeRange, value: cellValue };
 
       // inject column value references
       _.forEach(formula.paramsCols, function (formulaParamCol) {
@@ -459,25 +460,25 @@ function EnhancedTableVisController ($scope, Private, config) {
     return newColumn;
   };
 
-  const createComputedCell = function (column, row, totalHits, table) {
-    const value = computeFormulaValue(column.formula, row, totalHits, table);
+  const createComputedCell = function (table, column, row, totalHits, timeRange) {
+    const value = computeFormulaValue(column.formula, table, row, totalHits, timeRange);
     const parent = row.length > 0 && row[row.length-1];
     const newCell = new AggConfigResult(column.aggConfig, parent, value, value);
     newCell.column = column;
     if (column.template !== undefined) {
-      newCell.templateContext = createTemplateContext(column, row, totalHits, table);
+      newCell.templateContext = createTemplateContext(table, column, row, totalHits, timeRange);
     }
     if (column.cellComputedCssFormula !== undefined) {
-      newCell.cssStyle = computeFormulaValue(column.cellComputedCssFormula, row, totalHits, table, value);
+      newCell.cssStyle = computeFormulaValue(column.cellComputedCssFormula, table, row, totalHits, timeRange, value);
     }
     newCell.toString = renderCell;
     return newCell;
   };
 
-  const addComputedColumnToTables = function (tables, newColumn, customColumnPosition, totalHits) {
+  const addComputedColumnToTables = function (tables, newColumn, customColumnPosition, totalHits, timeRange) {
     _.forEach(tables, function (table) {
       if (table.tables) {
-        addComputedColumnToTables(table.tables, newColumn, customColumnPosition, totalHits);
+        addComputedColumnToTables(table.tables, newColumn, customColumnPosition, totalHits, timeRange);
         return;
       }
 
@@ -490,7 +491,7 @@ function EnhancedTableVisController ($scope, Private, config) {
         table.columns.push(newColumn);
       }
       _.forEach(table.rows, function (row) {
-        const newCell = createComputedCell(newColumn, row, totalHits, table);
+        const newCell = createComputedCell(table, newColumn, row, totalHits, timeRange);
         if (customColumnPosition || customColumnPosition === 0) {
           row.splice(customColumnPosition, 0, newCell);
         }
@@ -502,50 +503,50 @@ function EnhancedTableVisController ($scope, Private, config) {
 
       // compute total if totalFormula is present
       if (newColumn.totalFormula) {
-        newColumn.total = computeFormulaValue(newColumn.totalFormula, null, totalHits, table);
+        newColumn.total = computeFormulaValue(newColumn.totalFormula, table, null, totalHits, timeRange);
       }
 
     });
   };
 
-  const processLinesComputedFilter = function (tables, linesComputedFilterFormula, totalHits) {
+  const processLinesComputedFilter = function (tables, linesComputedFilterFormula, totalHits, timeRange) {
     return _.filter(tables, function (table) {
       if (table.tables) {
-        table.tables = processLinesComputedFilter(table.tables, linesComputedFilterFormula, totalHits);
+        table.tables = processLinesComputedFilter(table.tables, linesComputedFilterFormula, totalHits, timeRange);
         return table.tables.length > 0;
       }
 
       table.rows = _.filter(table.rows, function (row) {
-        return computeFormulaValue(linesComputedFilterFormula, row, totalHits, table);
+        return computeFormulaValue(linesComputedFilterFormula, table, row, totalHits, timeRange);
       });
       return table.rows.length > 0;
     });
   };
 
-  const processRowsComputedCss = function (table, rowsComputedCssFormula, totalHits) {
+  const processRowsComputedCss = function (table, rowsComputedCssFormula, totalHits, timeRange) {
     if (table.tables) {
       table.tables.forEach(function(subTable) {
-        processRowsComputedCss(subTable, rowsComputedCssFormula, totalHits);
+        processRowsComputedCss(subTable, rowsComputedCssFormula, totalHits, timeRange);
       });
     }
     else {
       table.rows.forEach(function (row) {
-        row.cssStyle = computeFormulaValue(rowsComputedCssFormula, row, totalHits, table);
+        row.cssStyle = computeFormulaValue(rowsComputedCssFormula, table, row, totalHits, timeRange);
       });
     }
   };
 
-  const processRowsComputedOptions = function (tableGroups, columns, params, splitColIndex, totalHits) {
+  const processRowsComputedOptions = function (tableGroups, columns, params, splitColIndex, totalHits, timeRange) {
     // process lines computed filter
     if (params.linesComputedFilter) {
       const linesComputedFilterFormula = createFormula(params.linesComputedFilter, 'Rows computed filter', splitColIndex, columns, params.totalFunc);
-      tableGroups.tables = processLinesComputedFilter(tableGroups.tables, linesComputedFilterFormula, totalHits);
+      tableGroups.tables = processLinesComputedFilter(tableGroups.tables, linesComputedFilterFormula, totalHits, timeRange);
     }
 
     // process rows computed CSS
     if (params.rowsComputedCss) {
       const rowsComputedCssFormula = createFormula(params.rowsComputedCss, 'Rows computed CSS', splitColIndex, columns, params.totalFunc);
-      processRowsComputedCss(tableGroups, rowsComputedCssFormula, totalHits);
+      processRowsComputedCss(tableGroups, rowsComputedCssFormula, totalHits, timeRange);
     }
   };
 
@@ -654,12 +655,12 @@ function EnhancedTableVisController ($scope, Private, config) {
 
   const DEFAULT_METRIC_VALUE = 0;
 
-  const splitCols = function (table, splitColIndex, totalHits) {
+  const splitCols = function (table, splitColIndex, totalHits, timeRange) {
 
     // process only real tables (with rows)
     if (table.tables) {
       _.forEach(table.tables, function (table) {
-        splitCols(table, splitColIndex, totalHits);
+        splitCols(table, splitColIndex, totalHits, timeRange);
       });
       return;
     }
@@ -669,7 +670,7 @@ function EnhancedTableVisController ($scope, Private, config) {
     for (let i = 0; i < refRowForComputedColumn.length; i++) {
       const cell = refRowForComputedColumn[i];
       if (cell.column !== undefined) {
-        refRowForComputedColumn[i] = createComputedCell(cell.column, refRowForComputedColumn, totalHits, table);
+        refRowForComputedColumn[i] = createComputedCell(table, cell.column, refRowForComputedColumn, totalHits, timeRange);
       }
       else if (cell.type === 'metric') {
         refRowForComputedColumn[i] = new AggConfigResult(cell.aggConfig, null, DEFAULT_METRIC_VALUE, DEFAULT_METRIC_VALUE, cell.filters);
@@ -729,7 +730,7 @@ function EnhancedTableVisController ($scope, Private, config) {
             newColDefaultMetric = new AggConfigResult(row[i].aggConfig, null, DEFAULT_METRIC_VALUE, DEFAULT_METRIC_VALUE, row[i].filters);
           }
           else {
-            newColDefaultMetric = createComputedCell(newCol, refRowForComputedColumn, totalHits, table);
+            newColDefaultMetric = createComputedCell(table, newCol, refRowForComputedColumn, totalHits, timeRange);
           }
           newColDefaultMetrics.push(newColDefaultMetric);
           for (let j = 0; j < newRows.length - 1; j++) {
@@ -959,6 +960,8 @@ function EnhancedTableVisController ($scope, Private, config) {
         const totalHits = $scope.esResponse.totalHits;
         const vis = $scope.vis;
         const params = $scope.visParams;
+        const aggs = $scope.esResponse.aggs;
+        const timeRange = computeTimeRange(aggs.timeRange, getConfig('dateFormat:dow'));
 
         // set the full AggConfig object on each split table and each column
         setFullAggConfig(tableGroups, vis.aggs);
@@ -983,20 +986,20 @@ function EnhancedTableVisController ($scope, Private, config) {
 
         // process 'Split cols' bucket: transform rows to cols
         if (splitColIndex !== -1 && !params.computedColsPerSplitCol) {
-          splitCols(tableGroups, splitColIndex, totalHits);
+          splitCols(tableGroups, splitColIndex, totalHits, timeRange);
         }
 
         // add computed columns
         _.forEach(params.computedColumns, function (computedColumn, index) {
           if (computedColumn.enabled) {
             const newColumn = createColumn(computedColumn, index, totalHits, splitColIndex, firstTable.columns, params.showTotal, params.totalFunc);
-            addComputedColumnToTables(tableGroups.tables, newColumn, computedColumn.customColumnPosition, totalHits);
+            addComputedColumnToTables(tableGroups.tables, newColumn, computedColumn.customColumnPosition, totalHits, timeRange);
           }
         });
 
         // process rows computed options : lines computed filter and rows computed CSS (no split cols)
         if (splitColIndex === -1) {
-          processRowsComputedOptions(tableGroups, firstTable.columns, params, splitColIndex, totalHits);
+          processRowsComputedOptions(tableGroups, firstTable.columns, params, splitColIndex, totalHits, timeRange);
         }
 
         // remove hidden columns
@@ -1007,12 +1010,12 @@ function EnhancedTableVisController ($scope, Private, config) {
         // process 'Split cols' bucket: transform rows to cols
         if (splitColIndex !== -1 && params.computedColsPerSplitCol) {
           splitColIndex = findSplitColIndex(firstTable);
-          splitCols(tableGroups, splitColIndex, totalHits);
+          splitCols(tableGroups, splitColIndex, totalHits, timeRange);
         }
 
         // process rows computed options : lines computed filter and rows computed CSS (split cols)
         if (splitColIndex !== -1) {
-          processRowsComputedOptions(tableGroups, firstTable.columns, params, -1, totalHits);
+          processRowsComputedOptions(tableGroups, firstTable.columns, params, -1, totalHits, timeRange);
         }
 
         // add total label
---
+++ public/time_range_computer.ts
@@ -0,0 +1,194 @@
+interface TimeRangeInput {
+  from: string;
+  to: string;
+}
+
+interface TimeRangeOutput {
+  duration: any;
+  from: any;
+  to: any;
+}
+
+function incrementDateUnit(date: Date, incrementCount: number, unit: string) {
+  if (unit === 'y') {
+    date.setFullYear(date.getFullYear() + incrementCount);
+  }
+  else if (unit === 'M') {
+    date.setMonth(date.getMonth() + incrementCount);
+  }
+  else if (unit === 'w') {
+    date.setDate(date.getDate() + incrementCount * 7);
+  }
+  else if (unit === 'd') {
+    date.setDate(date.getDate() + incrementCount);
+  }
+  else if (unit === 'h' || unit === 'H') {
+    date.setHours(date.getHours() + incrementCount);
+  }
+  else if (unit === 'm') {
+    date.setMinutes(date.getMinutes() + incrementCount);
+  }
+  else if (unit === 's') {
+    date.setSeconds(date.getSeconds() + incrementCount);
+  }
+}
+
+function parseDathMathExpression(dateMathExpression: string, roundUp: boolean, nowReference: number, dayOfWeekNumber: number): Date {
+
+  // extract rounding part
+  const roundingIndex: number = dateMathExpression.indexOf('/');
+  let roundUnit: string;
+  if (roundingIndex !== -1) {
+    roundUnit = dateMathExpression.substring(roundingIndex + 1);
+    dateMathExpression = dateMathExpression.substring(0, roundingIndex);
+  }
+
+  // initiate result date
+  const date: Date = new Date(nowReference);
+
+  // update date with +/- intervals
+  const matches = dateMathExpression.match(/([\+\-][0-9]+[yMwdHhms])/g);
+  if (matches) {
+    matches.forEach(match => {
+      const multiplier = parseInt(match.substring(0, 1) + '1');
+      const incrementCount = parseInt(match.substring(1, match.length - 1)) * multiplier;
+      const unit = match.substring(match.length - 1);
+      incrementDateUnit(date, incrementCount, unit);
+    });
+  }
+
+  // round date
+  if (roundUnit) {
+    if (roundUp) {
+      incrementDateUnit(date, 1, roundUnit);
+    }
+    if (roundUnit.match(/[y]/)) {
+      date.setMonth(1);
+    }
+    if (roundUnit.match(/[yM]/)) {
+      date.setDate(1);
+    }
+    if (roundUnit.match(/[w]/)) {
+      const dayOfWeekToDay = Math.abs(date.getDay() - dayOfWeekNumber);
+      date.setDate(date.getDate() - dayOfWeekToDay);
+    }
+    if (roundUnit.match(/[yMwd]/)) {
+      date.setHours(0);
+    }
+    if (roundUnit.match(/[yMwdHh]/)) {
+      date.setMinutes(0);
+    }
+    if (roundUnit.match(/[yMwdHhm]/)) {
+      date.setSeconds(0);
+    }
+    if (roundUnit.match(/[yMwdHhms]/)) {
+      date.setMilliseconds(0);
+    }
+    if (roundUp) {
+      date.setMilliseconds(date.getMilliseconds() - 1);
+    }
+  }
+
+  // return result date
+  return date;
+}
+
+function parseDate(date: string, roundUp: boolean, nowReference: number, dayOfWeekNumber: number): Date {
+  if (date.indexOf('now') === 0) {
+    return parseDathMathExpression(date.substring(3), roundUp, nowReference, dayOfWeekNumber);
+  }
+  else {
+    return new Date(date);
+  }
+}
+
+function computeDuration(from: Date, to: Date): any {
+  const milliseconds = to.getTime() - from.getTime();
+  const seconds = Math.ceil(milliseconds / 1000);
+  const minutes = Math.ceil(seconds / 60) + (from.getTimezoneOffset() - to.getTimezoneOffset());
+  const hours = Math.ceil(minutes / 60);
+  const days = Math.ceil(hours / 24);
+  const weeks = Math.ceil(days / 7);
+
+  let months = (to.getFullYear() - from.getFullYear()) * 12 + (to.getMonth() - from.getMonth());
+  if (months === 0 || to.getDate() > from.getDate()) {
+    months += 1;
+  }
+
+  let years = to.getFullYear() - from.getFullYear();
+  if (years === 0 || to.getMonth() > from.getMonth()) {
+    years += 1;
+  }
+
+  return { milliseconds, seconds, minutes, hours, days, weeks, months, years };
+}
+
+function getDayOfWeekNumber(dayOfWeek?: string): number {
+  if (dayOfWeek === 'Monday') {
+    return 1;
+  }
+  else if (dayOfWeek === 'Tuesday') {
+    return 2;
+  }
+  else if (dayOfWeek === 'Wednesday') {
+    return 3;
+  }
+  else if (dayOfWeek === 'Thursday') {
+    return 4;
+  }
+  else if (dayOfWeek === 'Friday') {
+    return 5;
+  }
+  else if (dayOfWeek === 'Saturday') {
+    return 6;
+  }
+  else {
+    return 0;
+  }
+}
+
+function computeDateStructure(date: Date): any {
+  return {
+    fullYear: date.getFullYear(),
+    month: date.getMonth(),
+    date: date.getDate(),
+    day: date.getDay(),
+    hours: date.getHours(),
+    minutes: date.getMinutes(),
+    seconds: date.getSeconds(),
+    milliseconds: date.getMilliseconds(),
+    time: date.getTime(),
+    timezoneOffset: date.getTimezoneOffset(),
+    dateString: date.toDateString(),
+    isoString: date.toISOString(),
+    localeDateString: date.toLocaleDateString(),
+    localeString: date.toLocaleString(),
+    localeTimeString: date.toLocaleTimeString(),
+    string: date.toString(),
+    timeString: date.toTimeString(),
+    utcString: date.toUTCString()
+  };
+}
+
+/**
+ * compute time range between 'from' and 'to' (in time picker),
+ * and return a structure containing all informations on 'from' Date, 'to' Date, and time range duration
+ */
+export function computeTimeRange(timeRange?: TimeRangeInput, dayOfWeek?: string): TimeRangeOutput | undefined {
+
+  if (!timeRange) {
+    return undefined;
+  }
+
+  const nowReference: number = Date.now();
+  const dayOfWeekNumber: number = getDayOfWeekNumber(dayOfWeek);
+  const from: Date = parseDate(timeRange.from, false, nowReference, dayOfWeekNumber);
+  const to: Date = parseDate(timeRange.to, true, nowReference, dayOfWeekNumber);
+
+  return {
+    duration: computeDuration(from, to),
+    from: computeDateStructure(from),
+    to: computeDateStructure(to)
+  }
+
+}
