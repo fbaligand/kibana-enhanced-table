29246cd2d70c3e1c4dcbcb9a4530d3e307910e17
fbaligand <fbaligand@gmail.com>
2021-12-22 23:44:39 +0100

    fix case where totalHits=0 but table is not empty

--- public/enhanced-table-vis-controller.js
+++ public/enhanced-table-vis-controller.js
@@ -967,7 +967,7 @@ function EnhancedTableVisController ($scope, tableConfig) {
         }
 
         // no data to display
-        if (totalHits === 0 || firstTable === null) {
+        if (firstTable === null || firstTable.rows.length === 0) {
           $scope.hasSomeRows = false;
           $scope.hasSomeData = false;
           $scope.renderComplete();
