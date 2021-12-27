c95885bee05c6d1e968c3ce946de53d3f2096d25
fbaligand <fbaligand@gmail.com>
2021-12-28 00:38:52 +0100

    fix timeRange computing when from/to date contains '||'

--- public/time_range_computer.ts
+++ public/time_range_computer.ts
@@ -49,11 +49,11 @@ function parseDathMathExpression(dateMathExpression: string, roundUp: boolean, n
   const date: Date = new Date(nowReference);
 
   // update date with +/- intervals
-  const matches = dateMathExpression.match(/([\+\-][0-9]+[yMwdHhms])/g);
+  const matches = dateMathExpression.match(/([+-/][0-9]*[yMwdHhms])/g);
   if (matches) {
     matches.forEach(match => {
-      const multiplier = parseInt(match.substring(0, 1) + '1');
-      const incrementCount = parseInt(match.substring(1, match.length - 1)) * multiplier;
+      const multiplier = parseInt(match.substring(0, 1) + '1', 10);
+      const incrementCount = parseInt(match.substring(1, match.length - 1), 10) * multiplier;
       const unit = match.substring(match.length - 1);
       incrementDateUnit(date, incrementCount, unit);
     });
@@ -95,12 +95,14 @@ function parseDathMathExpression(dateMathExpression: string, roundUp: boolean, n
   return date;
 }
 
-function parseDate(date: string | any, roundUp: boolean, nowReference: number, dayOfWeekNumber: number): Date {
-  if (typeof date === 'string' && date.indexOf('now') === 0) {
+function parseDate(date: string, roundUp: boolean, nowReference: number, dayOfWeekNumber: number): Date {
+  if (date.startsWith('now')) {
     return parseDathMathExpression(date.substring(3), roundUp, nowReference, dayOfWeekNumber);
   }
-  else if (date.toDate !== undefined) {
-    return date.toDate();
+  else if (date.indexOf('||') !== -1) {
+    const doublePipeIndex: number = date.indexOf('||');
+    const dateReference: Date = new Date(date.substring(0, doublePipeIndex));
+    return parseDathMathExpression(date.substring(doublePipeIndex + 2), roundUp, dateReference.getTime(), dayOfWeekNumber);
   }
   else {
     return new Date(date);
@@ -194,6 +196,6 @@ export function computeTimeRange(timeRange?: TimeRangeInput, dayOfWeek?: string)
     duration: computeDuration(from, to),
     from: computeDateStructure(from),
     to: computeDateStructure(to)
-  }
+  };
 
 }
