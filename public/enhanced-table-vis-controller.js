795067bade4bd58d469bd835b63d9f9294b422f2
fbaligand <fbaligand@gmail.com>
2022-01-01 23:57:55 +0100

    fix col reference by label with 'Compute total using formula'

--- public/enhanced-table-vis-controller.js
+++ public/enhanced-table-vis-controller.js
@@ -451,8 +451,7 @@ function EnhancedTableVisController ($scope, config) {
 
     // process "computeTotalUsingFormula" option
     if (showTotal && computedColumn.computeTotalUsingFormula) {
-      const totalFormula = computedColumn.formula.replace(/col(\[|\d+)/g, 'total$1')
-        .replace(/col\s*\(\s*(\d+)[^)]*\)/g, 'total$1');
+      const totalFormula = computedColumn.formula.replace(/col(\[|\s*\(|\d+)/g, 'total$1');
       newColumn.totalFormula = createFormula(totalFormula, 'computed total', splitColIndex, columns, totalFunc);
     }
 
