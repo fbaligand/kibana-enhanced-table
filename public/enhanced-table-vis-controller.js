f24e0ddde5e2d25baee869ad51f3cd682265fcfe
fbaligand <fbaligand@gmail.com>
2022-01-27 09:58:56 +0100

    fix col() function used with customColPosition (issue #243)

--- public/enhanced-table-vis-controller.js
+++ public/enhanced-table-vis-controller.js
@@ -545,14 +545,6 @@ module.controller('EnhancedTableVisController', function ($scope, Private, confi
 
     // add new computed column
     newColumn = _.clone(newColumn);
-    if (customColumnPosition || customColumnPosition === 0) {
-      table.columns.splice(customColumnPosition, 0, newColumn);
-    }
-    else {
-      table.columns.push(newColumn);
-    }
-
-    // add "total" formatter function
     if (!computedColsPerSplitCol) {
       newColumn.totalFormatter = createTotalFormatter(table, newColumn, undefined, totalHits, timeRange, computedColsPerSplitCol, splitColIndex);
     }
@@ -572,6 +564,14 @@ module.controller('EnhancedTableVisController', function ($scope, Private, confi
     // compute total if totalFormula is present
     if (newColumn.totalFormula && !computedColsPerSplitCol) {
       newColumn.total = computeFormulaValue(newColumn.totalFormula, table, null, totalHits, timeRange);
+    }
+
+    // insert new column in table
+    if (customColumnPosition || customColumnPosition === 0) {
+      table.columns.splice(customColumnPosition, 0, newColumn);
+    }
+    else {
+      table.columns.push(newColumn);
     }
 
   };
