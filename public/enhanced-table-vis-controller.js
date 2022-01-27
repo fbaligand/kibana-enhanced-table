54ae0d9a6ee834a351d4da6a8da2c3443b7cd93d
fbaligand <fbaligand@gmail.com>
2022-01-27 09:58:56 +0100

    fix col() function used with customColPosition (issue #243)

--- public/enhanced-table-vis-controller.js
+++ public/enhanced-table-vis-controller.js
@@ -541,15 +541,11 @@ module.controller('EnhancedTableVisController', function ($scope, Private, confi
     newColumn = _.clone(newColumn);
     newColumn.rawData = { table: { columns: [ newColumn] }, row: -1, column: 0 };
 
-    // process special "custom column position" case
-    if (customColumnPosition || customColumnPosition === 0) {
-      table.columns.splice(customColumnPosition, 0, newColumn);
-    }
-    else {
-      table.columns.push(newColumn);
+    if (!computedColsPerSplitCol) {
+      newColumn.totalFormatter = createTotalFormatter(table, newColumn, undefined, totalHits, timeRange, computedColsPerSplitCol, splitColIndex);
     }
 
-    // add computed cells
+    // add new computed column cells
     _.forEach(table.rows, function (row) {
       const newCell = createComputedCell(table, newColumn, row, totalHits, timeRange, computedColsPerSplitCol, splitColIndex);
       if (customColumnPosition || customColumnPosition === 0) {
@@ -563,6 +559,14 @@ module.controller('EnhancedTableVisController', function ($scope, Private, confi
     // compute total if totalFormula is present
     if (newColumn.totalFormula && !computedColsPerSplitCol) {
       newColumn.total = computeFormulaValue(newColumn.totalFormula, table, null, totalHits, timeRange);
+    }
+
+    // insert new column in table
+    if (customColumnPosition || customColumnPosition === 0) {
+      table.columns.splice(customColumnPosition, 0, newColumn);
+    }
+    else {
+      table.columns.push(newColumn);
     }
 
   };
