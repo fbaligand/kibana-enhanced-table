fa8691137eb608924ee512136b9c267aecac21bf
kobi2c <kobi@cortexmg.com>
2022-08-25 14:21:57 +0300

    add percentChange

--- public/parser/percents.ts
+++ public/parser/percents.ts
@@ -1,8 +1,11 @@
 export function addParser(parser) {
-  parser.functions.percentFrom = function percentFrom (num,from) {
+  parser.functions.percentFrom = function percentFrom (num: number,from: number) : number{
     return from >= 0 ? num * 100 / from : 0;
   }
-  parser.functions.percentOf = function percentOf(num,percent) {
+  parser.functions.percentOf = function percentOf(num: number,percent: number) : number {
     return num*percent / 100;
+  }
+  parser.functions.percentChange = function percentOf(valNew: number,valOld: number) : number{
+    return parser.functions.percentFrom(valNew-valOld, valOld)
   }
 }
