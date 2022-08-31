11585767fb22c926f1d371f677ae36507ef3af6d
fbaligand <fbaligand@gmail.com>
2022-08-31 09:16:08 +0200

    document-table: replace '_id' sort by '_doc' sort

--- public/data_load/enhanced-table-request-handler.js
+++ public/data_load/enhanced-table-request-handler.js
@@ -70,7 +70,7 @@ export async function enhancedTableRequestHandler ({
       }
     }]);
     if (visParams.hitsSize !== undefined && visParams.hitsSize > MAX_HITS_SIZE) {
-      searchSource.getField('sort').push({'_id': {'order': 'asc','unmapped_type': 'keyword'}});
+      searchSource.getField('sort').push({'_doc': {}});
     }
   }
 
