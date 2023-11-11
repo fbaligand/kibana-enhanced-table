2b85884f7652f0254ab10208eda401fb7f79438a
fbaligand <fbaligand@gmail.com>
2023-11-11 16:13:38 +0100

    add support for runtime fields in document table

--- public/data_load/enhanced-table-request-handler.js
+++ public/data_load/enhanced-table-request-handler.js
@@ -31,7 +31,7 @@ export async function enhancedTableRequestHandler ({
     if (!visParams.fieldColumns.some(fieldColumn => fieldColumn.field.name === '_source')) {
       searchSource.setField('_source', visParams.fieldColumns.map(fieldColumn => fieldColumn.field.name));
     }
-    searchSource.setField('docvalue_fields', visParams.fieldColumns.filter(fieldColumn => fieldColumn.field.readFromDocValues).map(fieldColumn => fieldColumn.field.name));
+    searchSource.setField('docvalue_fields', visParams.fieldColumns.filter(fieldColumn => fieldColumn.field.aggregatable).map(fieldColumn => fieldColumn.field.name));
     const scriptFields = {};
     visParams.fieldColumns.filter(fieldColumn => fieldColumn.field.scripted).forEach(fieldColumn => {
       scriptFields[fieldColumn.field.name] = {
