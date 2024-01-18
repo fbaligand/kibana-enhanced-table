d00829b2ae0b23217890e1e6a863cd7b4554a095
fbaligand <fbaligand@gmail.com>
2024-01-18 23:34:09 +0100

    add 'rawValue' variable to template

--- public/enhanced-table-vis-controller.js
+++ public/enhanced-table-vis-controller.js
@@ -451,6 +451,7 @@ function EnhancedTableVisController ($scope, Private, config) {
     let result = this.column.fieldFormatter.convert(this.value);
     if (this.templateContext !== undefined) {
       this.templateContext.value = result;
+      this.templateContext.rawValue = this.value;
       result = this.column.template.compiledTemplate(this.templateContext);
     }
     if (contentType !== 'html') {
