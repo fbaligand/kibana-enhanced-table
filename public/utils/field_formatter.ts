e0f3667fb42225fb0650d4d0312a38e7889e062c
fbaligand <fbaligand@gmail.com>
2024-01-21 21:40:04 +0100

    add 'utils' sub package

--- public/utils/field_formatter.ts
+++ public/utils/field_formatter.ts
@@ -1,6 +1,6 @@
 import { IAggConfig } from '@kbn/data-plugin/public';
 import { IFieldFormat, FieldFormatsContentType } from '@kbn/field-formats-plugin/common';
-import { getFormatService } from './services';
+import { getFormatService } from '../services';
 
 /**
  * Returns the fieldFormatter function associated to aggConfig, for the requested contentType (html or text).
