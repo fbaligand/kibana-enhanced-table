0a3e4332eee4e8d7f2d6a4f3c2ae30b535250a10
fbaligand <fbaligand@gmail.com>
2024-01-21 21:40:04 +0100

    add 'utils' sub package

--- public/utils/field_formatter.ts
+++ public/utils/field_formatter.ts
@@ -1,5 +1,5 @@
-import { getFormatService } from './services';
-import { IAggConfig, IFieldFormat, FieldFormatsContentType } from '../../../src/plugins/data/public';
+import { getFormatService } from '../services';
+import { IAggConfig, IFieldFormat, FieldFormatsContentType } from '../../../../src/plugins/data/public';
 
 /**
  * Returns the fieldFormatter function associated to aggConfig, for the requested contentType (html or text).
